c1cbf2d64e04e83ba74b8124dc6fe2aee21dd8e0
VictorMarinho97 <victormarinho.ads1@gmail.com>
2022-10-13 19:53:46 -0300

    Initial commit

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons'
+import { themes } from '@storybook/theming' /*importar temas e addons para o storybook*/ 
+
+addons.setConfig({
+    theme: themes.dark,
+})
---
+++ storybook-static/assets/preview.792f8fc2.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.f87ef7f3.js";import"./iframe.72f44da8.js";import"./es.map.constructor.dee6d35d.js";import"./es.number.to-fixed.52ee3426.js";import"./index.5c656140.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.792f8fc2.js.map
